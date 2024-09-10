f04dcc96fde1e598895b6aa9a16b9f2f7f81a868
dynio7 <kglowiak4@gmail.com>
2024-09-10 20:54:36 +0200

    Poprawa działania rejestracji. Dodanie błędów walidacyjnych przy tworzeniu konta.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using Magazyn.Data;
 using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 namespace Magazyn
 {
@@ -17,10 +20,39 @@ namespace Magazyn
             .AddEntityFrameworkStores<MagazynContext>()
             .AddDefaultTokenProviders();
 
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            builder.Services.Configure<RequestLocalizationOptions>(options => 
+            {
+                var supportedCultures = new[] { new CultureInfo("pl") };
+                options.DefaultRequestCulture = new RequestCulture("pl");
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+
+            builder.Services.AddControllersWithViews().
+                AddDataAnnotationsLocalization().
+                AddViewLocalization();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+            });
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            var localizationOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+            app.UseRequestLocalization(localizationOptions);
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -29,41 +61,6 @@ namespace Magazyn
                 app.UseHsts();
             }
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-                string[] roleNames = { "Admin", "User" }; // Przykładowe role
-
-                foreach (var roleName in roleNames)
-                {
-                    var roleExist = await roleManager.RoleExistsAsync(roleName);
-                    if (!roleExist)
-                    {
-                        await roleManager.CreateAsync(new IdentityRole(roleName));
-                    }
-                }
-
-                // Tworzenie domyślnego użytkownika z rolą Admin
-                var adminUser = new IdentityUser
-                {
-                    UserName = "admin@example.com",
-                    Email = "admin@example.com"
-                };
-
-                string adminPassword = "Admin123!"; // Pamiętaj o zasadach bezpieczeństwa przy tworzeniu haseł
-
-                var user = await userManager.FindByEmailAsync(adminUser.Email);
-                if (user == null)
-                {
-                    var createAdminUser = await userManager.CreateAsync(adminUser, adminPassword);
-                    if (createAdminUser.Succeeded)
-                    {
-                        await userManager.AddToRoleAsync(adminUser, "Admin");
-                    }
-                }
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
